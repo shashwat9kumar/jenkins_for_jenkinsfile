85cb5078169fba4d63a446b923200e2656dfda7e
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-08-25 11:57:37 +0530

    Add files via upload

---
+++ src/main/java/Test1.java
@@ -0,0 +1,20 @@
+public class Test1 {
+	private int n =10;
+	
+	public void say(int n1)
+	{
+		n1=20;
+	}
+	public static void main(String[] args) {
+		int n2 = 10;
+		
+		if( n2 == 20 )
+		{
+			System.out.println("This is false");
+		}
+		
+		System.out.println("Atleast");
+
+	}
+
+}
